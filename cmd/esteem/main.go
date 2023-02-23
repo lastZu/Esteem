a0f516620bf8644125550b0bcacf70a4e25fd1cd
lastKraken <cdrom2015@ya.ru>
2023-02-23 06:31:33 +0300

    init config, start logger

--- cmd/esteem/main.go
+++ cmd/esteem/main.go
@@ -2,11 +2,44 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+
+	"github.com/lastZu/Esteem/internal/app/utils"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
 )
 
 func main() {
+	flag.Parse()
+
+	config := utils.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engineer := utils.New(config)
+	if err := engineer.Start(); err != nil {
+		log.Fatal(err)
+	}
+
 	token := mustToken()
+
+	fmt.Println(engineer)
+	fmt.Println(token)
+}
+
+func init() {
+	flag.StringVar(
+		&configPath,
+		"config-path",
+		"configs/utils.toml",
+		"path to config file")
 }
 
 func mustToken() string {
