ea4d6b372a7401fec6f7bc87a353cef51fbce4d1
lastKraken <cdrom2015@ya.ru>
2023-02-23 04:39:20 +0300

    get token

--- cmd/esteem/main.go
+++ cmd/esteem/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+)
 
+func main() {
+	token := mustToken()
+}
+
+func mustToken() string {
+	token := flag.String(
+		"t",
+		"",
+		"token for access to telegram bot",
+	)
+	flag.Parse()
+	if *token == "" {
+		log.Fatal("wrong token")
+	}
+
+	return *token
 }
